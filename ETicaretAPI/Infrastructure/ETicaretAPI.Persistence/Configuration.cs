940142ce67272b9d103819faea77a5e8853362c7
OmerFarukBatur <44163877+OmerFarukBatur@users.noreply.github.com>
2022-03-22 20:05:09 +0300

    new created

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+       static public string ConfigurationString
+        {
+            get 
+            {
+                ConfigurationManager configurationManager = new();
+                //configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "./../../Presentation/ETicaretAPI.API"));
+                // bu kısımda dosya yolu uzantısı hatası var
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
---
+++ ETicaretAPI/Presenttation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProducReadRepository   _producReadRepository;
+
+        readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+
+        public ProductController(IProductWriteRepository productWriteRepository, 
+            IProducReadRepository producReadRepository, 
+            IOrderWriteRepository orderWriteRepository,
+            IOrderReadRepository orderReadRepository,
+            ICustomerWriteRepository customerWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _producReadRepository = producReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _orderReadRepository = orderReadRepository;
+            _customerWriteRepository = customerWriteRepository;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            Order order = await _orderReadRepository.GetByIdAsync("864b41fd-3ceb-42c3-95c0-b647d8769d53");
+            order.Address = "istanbul";
+            await _orderWriteRepository.SaveAsync();
+            
+        }
+
+    }
+}
