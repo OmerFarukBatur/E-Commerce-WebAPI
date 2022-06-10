eee94502dfa420a0b73dd0e1302de38d98e9c486
OmerFarukBATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2022-06-10 17:10:51 +0300

    Developed Identity Mechanism

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using U = ETicaretAPI.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Exceptions;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest, CreateUserCommandResponse>
+    {
+        readonly UserManager<U.AppUser> _userManager;
+
+        public CreateUserCommandHandler(UserManager<U.AppUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            IdentityResult result = await _userManager.CreateAsync(new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = request.UserName,
+                Email = request.Email,
+                NameSurname = request.NameSurname
+            }, request.Password);
+
+
+            CreateUserCommandResponse response = new() { Succeeded = true };
+
+            if (result.Succeeded)
+            {
+                response.Message = "Kullanıcı başarıyla oluşturulmuştur.";
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    response.Message += $"{error.Code} - {error.Description}\n";
+                }
+            }
+
+            return response;
+
+            //throw new UserCreateFailedException();
+        }
+    }
+}
