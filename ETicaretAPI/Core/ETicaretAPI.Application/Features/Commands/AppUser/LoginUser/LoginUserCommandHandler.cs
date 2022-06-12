0e224a6b0578b52003da4cc121be87c8674d1261
OmerFarukBatur <44163877+OmerFarukBatur@users.noreply.github.com>
2022-06-12 14:18:21 +0300

    Authentication done

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using U = ETicaretAPI.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Exceptions;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserCommandResponse>
+    {
+        readonly UserManager<U.AppUser> _userManager;
+        readonly SignInManager<U.AppUser> _signInManager;
+
+        public LoginUserCommandHandler(UserManager<U.AppUser> userManager, SignInManager<U.AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            U.AppUser appUser = await  _userManager.FindByNameAsync(request.UserNameOrEmail);
+            if (appUser == null)
+                appUser = await _userManager.FindByEmailAsync(request.UserNameOrEmail);
+            
+            if (appUser == null)
+                throw new NotFoundUserException();
+
+            SignInResult result = await _signInManager.CheckPasswordSignInAsync(appUser, request.Password,false);
+
+            if (result.Succeeded) // Authentication başarılı
+            { 
+                //   Yetkileri belirlemeliyiz.
+                return new();
+            }
+
+            return null;
+        }
+    }
+}
